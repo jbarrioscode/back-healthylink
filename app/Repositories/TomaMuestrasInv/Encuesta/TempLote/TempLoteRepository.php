65144bd6456e3f15e8a81baeff42382dc12e6c57
JoseBarrios <jose.barrios@mibcode.com>
2024-04-26 20:58:46 -0500

    ADD TEMP

--- app/Repositories/TomaMuestrasInv/Encuesta/TempLote/TempLoteRepository.php
+++ app/Repositories/TomaMuestrasInv/Encuesta/TempLote/TempLoteRepository.php
@@ -2,7 +2,10 @@
 
 namespace App\Repositories\TomaMuestrasInv\Encuesta\TempLote;
 
+use App\Models\TomaMuestrasInv\Muestras\FormularioMuestra;
+use App\Models\TomaMuestrasInv\Muestras\SedesTomaMuestra;
 use App\Models\TomaMuestrasInv\Muestras\TempLote;
+use App\Models\User;
 use App\Traits\AuthenticationTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,6 +14,7 @@ use Illuminate\Support\Facades\Validator;
 class TempLoteRepository implements TempRepositoryInterface
 {
     use AuthenticationTrait;
+
     public function guardarLoteTemp(Request $request)
     {
 
@@ -19,17 +23,15 @@ class TempLoteRepository implements TempRepositoryInterface
         try {
 
             $rules = [
-                'minv_formulario_id' => 'required',
                 'user_id' => 'required',
-                'sede_id' => 'required',
-                'tipo_muestra' => 'required',
+                'codigo_muestra' => 'required',
+                'tipo_muestra' => 'required'
             ];
 
             $messages = [
-                'minv_formulario_id.required' => 'Muestra está vacio.',
+                'codigo_muestra.required' => 'Codigo muestra está vacio.',
                 'user_id.required' => 'ID usuario está vacio.',
-                'sede_id.required' => 'Sede está vacio.',
-                'tipo_muestra.required' => 'Tipo Muestra está vacio.',
+                'tipo_muestra.required' => 'Tipo muestra está vacio.',
             ];
 
             $validator = Validator::make($request->all(), $rules, $messages);
@@ -37,10 +39,20 @@ class TempLoteRepository implements TempRepositoryInterface
                 return $this->error($validator->errors(), 422, []);
             }
 
+            $codificacion = explode('-', $request->codigo_muestra);
+
+            $codigo_muestra = preg_split('/([0-9]+)/', $codificacion[0], -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+
+            $validacion = self::validarCodificacionLote($codificacion,$codigo_muestra, $request->tipo_muestra);
+            if ($validacion != "") {
+                return $this->error($validacion, 204, []);
+            }
+
+
             $temp = TempLote::create([
-                'minv_formulario_id' => $request->minv_formulario_id,
+                'minv_formulario_id' => $codigo_muestra[1],
                 'user_id' => $request->user_id,
-                'sede_id' => $request->sede_id,
+                'sede_id' => $codificacion[2],
                 'tipo_muestra' => $request->tipo_muestra,
                 'lote_cerrado' => 'false',
             ]);
@@ -58,22 +70,48 @@ class TempLoteRepository implements TempRepositoryInterface
 
     }
 
-    public function getLoteTemp(Request $request,$user_id,$sede_id)
+    public static function validarCodificacionLote($codificacion,$codigo_muestra ,$tipo_muestra)
+    {
+        if (!isset($codificacion[0]) || !isset($codificacion[1]) || !isset($codificacion[2]) || !isset($codificacion[3])) {
+            return 'Codigo invalido';
+        }
+
+        if ($codigo_muestra[0] !== 'MU' || $codigo_muestra[0] !== 'CM') return "Codigo invalido";
+
+        if ($codigo_muestra[0] === 'MU' && $tipo_muestra !== 'MUESTRA') return "El codigo no pertenece a 'muestra'";
+
+        if ($codigo_muestra[0] === 'CM' && $tipo_muestra !== 'CONTRAMUESTRA') return "El codigo no pertenece a 'contramuestra'";
+
+
+        if (!FormularioMuestra::where('id', $codigo_muestra[1])->exists()) return 'La muestra no existe';
+
+        if (!FormularioMuestra::where('code_paciente', $codificacion[1])->exists()) return 'El paciente no existe';
+
+        if (!FormularioMuestra::where('id', $codigo_muestra[1])->where('code_paciente', $codificacion[1])->exists()) return 'El paciente y la muestra no coinciden';
+
+        if (!SedesTomaMuestra::where('id', $codificacion[2])->exists()) return 'La sede no existe';
+
+        if (!User::where('id', $codificacion[3])->exists()) return 'El recolector no existe';
+
+        return '';
+    }
+
+    public function getLoteTemp(Request $request, $user_id, $sede_id)
     {
 
         try {
 
-            $tempMuestras=TempLote::where('sede_id',$sede_id)
-                    ->where('user_id',$user_id)
-                    ->where('lote_cerrado','false')
-                    ->where('tipo_muestra','MUESTRA')->get();
+            $tempMuestras = TempLote::where('sede_id', $sede_id)
+                ->where('user_id', $user_id)
+                ->where('lote_cerrado', 'false')
+                ->where('tipo_muestra', 'MUESTRA')->get();
 
-            $tempContramuestras=TempLote::where('sede_id',$sede_id)
-                    ->where('user_id',$user_id)
-                    ->where('lote_cerrado','false')
-                    ->where('tipo_muestra','CONTRAMUESTRA')->get();
+            $tempContramuestras = TempLote::where('sede_id', $sede_id)
+                ->where('user_id', $user_id)
+                ->where('lote_cerrado', 'false')
+                ->where('tipo_muestra', 'CONTRAMUESTRA')->get();
 
-            $temp=['temp_Muestras'=>$tempMuestras,'tempContraMuestras'=>$tempContramuestras];
+            $temp = ['temp_Muestras' => $tempMuestras, 'tempContraMuestras' => $tempContramuestras];
 
             if (count($temp) == 0) return $this->error('No hay muestras con este usuario en esta sede con apertura', 204, []);
 
@@ -105,9 +143,9 @@ class TempLoteRepository implements TempRepositoryInterface
             }
 
 
-            $temp=TempLote::where('minv_formulario_id',$request->minv_formulario_id)
-                ->where('tipo_muestra',$request->tipo_muestra)
-                ->where('lote_cerrado',false)->delete();
+            $temp = TempLote::where('minv_formulario_id', $request->minv_formulario_id)
+                ->where('tipo_muestra', $request->tipo_muestra)
+                ->where('lote_cerrado', false)->delete();
 
             return $this->success($temp, 1, 'Eliminado correctamente', 200);
 
