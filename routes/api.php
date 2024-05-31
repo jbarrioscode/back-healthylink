48444eac393fd57bbab75c8dce38d37ed3cd61b9
camilosalgadocastro <casc1902@hotmail.com>
2024-05-30 23:05:12 -0500

    ROUTES fixed 30052024 2

--- routes/api.php
+++ routes/api.php
@@ -106,17 +106,6 @@ Route::prefix('/v1')->group(function () {
         Route::get('/encuesta/get/tempmuestrassponsorbox/{biobanco_id}', [EncuestaController::class, 'getTempoBoxSponsor']);
         Route::post('/encuesta/post/enviarmuestrassponsor', [EncuestaController::class, 'enviarMuestrasSponsor']);
 
-/*
-    Route::post('/encuesta/post/registrarencuesta', [EncuestaController::class, 'crearEncuesta']);
-    Route::post('/encuesta/post/registrarinformacionhistoriaclinica', [EncuestaController::class, 'AgregarInformacionComplementaria']);
-    Route::post('/encuesta/post/asignarmuestrasalote', [EncuestaController::class, 'crearAsignacionAutomaticaAlote']);//UNA
-    Route::post('/encuesta/post/lotesentrasporte', [EncuestaController::class, 'lotesEntrasporte']);
-    Route::post('/encuesta/post/lotemuestrasrecibidasbiobanco', [EncuestaController::class, 'muestrasEntregadasBioBanco']);
-    Route::post('/encuesta/post/asignacionbiobanco', [EncuestaController::class, 'muestrasAsignadasAnevera']);//TRES
-    Route::post('/encuesta/post/asignacionMuestrasEnvio', [EncuestaController::class, 'muestrasAsignadasAcajaEnvio']);//CUATRO
-    Route::get('/encuesta/get/tempmuestrassponsorbox/{biobanco_id}', [EncuestaController::class, 'getTempoBoxSponsor']);
-    Route::post('/encuesta/post/enviarmuestrassponsor', [EncuestaController::class, 'enviarMuestrasSponsor']);//CINCO//
-*/
         Route::get('/encuesta/get/encuestasporestado/{estado?}', [EncuestaController::class, 'trazabilidadEncuestas']);
         Route::get('/encuesta/get/estadosencuesta/{encuesta_id}', [EncuestaController::class, 'trazabilidadFlujoEstadosEncuesta']);
         Route::get('/encuesta/get/encuestassincrf', [EncuestaController::class, 'getEncuestasForCRF']);
@@ -130,23 +119,13 @@ Route::prefix('/v1')->group(function () {
         Route::get('/encuesta/get/tempmuestras/{user_id}/{sede_id}', [TempLoteController::class, 'getLoteTemp']);
         Route::post('/encuesta/delete/tempmuestras', [TempLoteController::class, 'deleteTemp']);
 
-
-    //Route::post('/encuesta/post/tempmuestras', [TempLoteController::class, 'guardarLoteTemp']);//SEIS
-    //Route::get('/encuesta/get/tempmuestras/{user_id}/{sede_id}', [TempLoteController::class, 'getLoteTemp']);
-    //Route::post('/encuesta/delete/tempmuestras', [TempLoteController::class, 'deleteTemp']);
-
         /*--------------------------------------------------------------------------------*/
         /* DASHBOARD */
 
         Route::get('/encuesta/get/reportes/datadashboard', [ReportesController::class, 'getDataDashboard']);
         Route::get('/encuesta/get/reportes/databydate/{initdate}/{enddate}', [ReportesController::class, 'getDataForFecha']);
 
-
-    //Route::get('/encuesta/get/reportes/datadashboard', [ReportesController::class, 'getDataDashboard']);
-    //Route::get('/encuesta/get/reportes/datadashboard/{startDate}/{endDate}', [ReportesController::class, 'getDataForFecha']);
-
-   });
-    //});
+    });
 
 
     /* ------------------------------------------------------------------------------------
